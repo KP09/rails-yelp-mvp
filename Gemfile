801a87017cee57099146107a1ac53eac1c72230b
Kees Postma <kk.postma@gmail.com>
2017-05-24 12:10:56 +0100

    Adds faker gem. Creates MVP routes.

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'faker'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+5.times do
+  name = Faker::Company.name
+  address = Faker::Address.street_address
+  phone_number = Faker::PhoneNumber.cell_phone
+  category = Restaurant::CATEGORIES.sample
+  Restaurant.create!(name: name, address: address, phone_number: phone_number, category: category)
+end
