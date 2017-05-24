4e419fb578fe07eec3f29f312ed9eee198f57322
Kees Postma <kk.postma@gmail.com>
2017-05-24 16:59:54 +0100

    Attempts to embed new review form in restaurants show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant
 
-  def new
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
